1c11407817c05993012a366b57c51421ea721785
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-17 13:58:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    //creat clickalert function
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    //add eventlistener
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
+
